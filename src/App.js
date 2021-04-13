a3a108bea0d1ea75812e6beb161d5f550a1af626
swapnil <kocher1327@gmail.com>
2021-04-13 23:08:35 +0530

    Created Skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React,{Component }from 'react';
-class App extends Component{
-  render(){
-    return(
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
       <div>
-     Phone Directory
+        <div className="header">
+          Phone Directory
+      </div>
+
+        <button>Add</button>
+        <div>
+          <span>Name</span> <br />
+          <span>Phone</span>
+        </div>
       </div>
     );
   }
 }
-  
+
 export default App;
